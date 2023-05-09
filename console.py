566aa813df0e87bc5d6fc5ed86d3e59429deca3f
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-05-09 21:01:54 +0100

    Added File structure and script up to task 3

---
+++ console.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+This module is the console for the
+AirBnB clone project.
+"""
+import cmd
+
+
+class HbnbConsole(cmd.Cmd):
+    """This is the console class where
+    all console functions are defined."""
+    prompt = "(Hbnb) "
+
+    def do_EOF(self, line):
+        """
+        This defines the End of Line Function
+        That ends the running program
+        """
+        print("")
+        return True
+
+    def emptyline(self):
+        pass
+
+    def do_quit(self, line):
+        """Calling this function quits the console"""
+        return True
+
+
+if __name__ == "__main__":
+    HbnbConsole().cmdloop()
---
+++ models/base_model.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+This module contains and defines all
+common attributes/methods for other classes.
+"""
+import uuid
+from datetime import datetime
+
+class BaseModel():
+    """
+    BaseModel Class
+    """
+    def __init__(self):
+        """Initialization for the Base class"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """string representation of Base class Instance"""
+        clsname = self.__class__.__name__
+        args = [clsname, self.id, self.__dict__]
+        return "[{}] ({}) {}".format(*args)
+    
+    def save(self):
+        """This method updates the updated_at attribute"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Dictionary representation of Base instance"""
+        base_dicko = self.__dict__.copy()
+        base_dicko["__class__"] = self.__class__.__name__
+        # base_dicko['created_at'] = self.created_at.isoformat()
+        # base_dicko['updated_at'] = self.updated_at.isoformat()
+        # run through each item in base dicko, if date time
+        for k, v in base_dicko.items():
+            if isinstance(v, datetime):
+                base_dicko[k] = v.isoformat()
+            else:
+                base_dicko[k] = v
+        return base_dicko
+
+
+    
+# if __name__ == "__main__":
+#     bins1 = BaseModel()
+#     print(bins1)
+#     bins2 = BaseModel()
+#     print(bins2)
