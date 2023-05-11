dc6baea3c0087fc02521b9e510e80c5322ef9f89
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-05-11 23:06:54 +0100

    Added more test cases

--- models/base_model.py
+++ models/base_model.py
@@ -28,7 +28,7 @@ class BaseModel():
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """string representation of Base class Instance"""
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,30 +2,12 @@
 """
 This is the unit test module
 for base model
-    TEST CASES:
-    test if isinstance of BaseModel✅
-    test if isisntance has attr id✅
-    test if isisntance has attr created_at✅
-    test if isisntance has attr updated_at✅
-    test output of to_dict
-    test if id is of type string✅
-    test if isisntance has extra added attributes✅
-    values of extra added attributes are appropriate✅
-    test if created at is in isoformat
-    test if to_dict returns a dictionary
-    test if magic string Output is formatted right
-    test if string representation of instance is correct
-    test if ids generated from uuid is unique
-    test if type of id is string
-    test if datecreated is isinstance of datetime
-    test if dateupdated is instance of datetime
-    datecreated must be initially equal to updated_at
-    confirm that to_dict contains __class__
-    to_dict returns type dictionary
 """
 import unittest
 from uuid import uuid4
 from models.base_model import BaseModel
+from datetime import datetime
+
 
 class TestBaseModel(unittest.TestCase):
     """
@@ -73,7 +55,7 @@ class TestBaseModel(unittest.TestCase):
     def test_to_dict_returns_dict(self):
         chk = BaseModel()
         chk1 = chk.to_dict()
-        self.assertTrue(isinstance(chk1, dict))
+        self.assertIsInstance(chk1, dict)
 
     def test_id_type(self):
         """
@@ -81,7 +63,70 @@ class TestBaseModel(unittest.TestCase):
         """
         chk = BaseModel()
         chk_id = chk.id
-        self.assertTrue(isinstance(chk_id, str))
+        self.assertIsInstance(chk_id, str)
+
+    def test_print_instance(self):
+        """
+        test if print instance Output is formatted right
+        """
+        chk = BaseModel()
+        clsname = self.__class__.__name__
+        args = [clsname, self.id, self.__dict__]
+        expRes = print("[{}] ({}) {}".format(*args))
+        # self.assertTrue(isinstance(print(chk), str))
+        self.assertEqual(print(chk), expRes)
+        
+    def test_contains_class_(self):
+        """
+        confirm that to_dict contains __class__
+        """
+        chk = BaseModel()
+        self.assertTrue("__class__" in chk.to_dict())
+
+    def test_unique_ids(self):
+        """Test uuid returns unique values"""
+        chk1 = BaseModel()
+        chk2 = BaseModel()
+        chk3 = BaseModel()
+        self.assertNotEqual(chk1.id, chk2.id)
+        self.assertNotEqual(chk2.id, chk3.id)
+
+    def test_created_at_type(self):
+        """test created at type is datetime"""
+        chk = BaseModel()
+        self.assertIsInstance(chk.created_at, datetime)
+    
+    def test_updated_at_type(self):
+        """test updated at type is datetime"""
+        chk = BaseModel()
+        self.assertIsInstance(chk.created_at, datetime)
+
+    def test_created_updated_at_eq(self):
+        """
+        Test to see that created_at and updated_at
+        are equal at instance initiation
+        """
+        chk = BaseModel()
+        self.assertEqual(chk.created_at, chk.updated_at)
+
+    def test_to_dict_output(self):
+        """
+        test output of to_dict
+        """
+        chk = BaseModel()
+        chk.id = 123456
+        chk.name= "My_Model"
+        dtnow = datetime.now()
+        chk.created_at = chk.updated_at = dtnow
+        chk_dictresult = {
+            'id': 123456,
+            'created_at': dtnow.isoformat(),
+            'name': "My_Model",
+            'updated_at': dtnow.isoformat(),
+            '__class__': "BaseModel"
+        }
+        self.assertDictEqual(chk_dictresult, chk.to_dict())
+
         
 if __name__ == "__main__":
     unittest.main()
